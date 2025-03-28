95c5d749ad8b06eec826891bbac9c675f72da845
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-03-28 15:22:27 -0300

    first story for button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,79 @@
+import { cn } from "@/utils";
+import { cva, VariantProps } from "class-variance-authority";
 import { ComponentProps } from "react";
 
-type ButtonProps = ComponentProps<"button">;
+const buttonStyles = cva(
+  [
+    "flex",
+    "items-center",
+    "justify-center",
+    "gap-2",
+    "rounded-full",
+    "font-medium",
+    "cursor-pointer",
+    "transition-transform",
+    "duration-200",
+    "ease-out",
+    "focus:outline-none",
+    "disabled:cursor-not-allowed",
+    "hover:scale-90",
+  ],
+  {
+    variants: {
+      variant: {
+        solid:
+          "bg-gradient-to-r from-brand-purple to-brand-purpleDark text-white shadow-md",
+        transparent:
+          "bg-transparent border border-brand-purple text-brand-purple hover:bg-brand-purple/10 shadow-md",
+      },
+      size: {
+        sm: "text-sm px-4 py-2",
+        md: "px-6 py-3",
+        lg: "text-lg px-8 py-4",
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "solid",
+        size: "sm",
+      },
+      {
+        variant: "transparent",
+        size: "sm",
+      },
+      {
+        variant: "solid",
+        size: "md",
+      },
+      {
+        variant: "transparent",
+        size: "md",
+      },
+      {
+        variant: "solid",
+        size: "lg",
+      },
+      {
+        variant: "transparent",
+        size: "lg",
+      },
+    ],
+    defaultVariants: {
+      variant: "solid",
+      size: "md",
+    },
+  }
+);
 
-export const Button = ({ ...props }: ButtonProps) => {
-  return <button className="bg-blue-500 text-white p-2 rounded" {...props} />;
+type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
+
+export const Button = ({ variant, size, className, ...props }: ButtonProps) => {
+  return (
+    <button
+      className={cn(buttonStyles({ variant, size, className }))}
+      {...props}
+    />
+  );
 };
+
+export default Button;
