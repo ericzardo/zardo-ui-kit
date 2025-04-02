695782390c4294c6a0c1055fe68dbb6a72fda8a0
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-04-02 20:06:50 -0300

    change logos path

--- src/assets/logos/index.ts
+++ src/assets/logos/index.ts
@@ -1,7 +1,14 @@
-export const AppleTouchIcon = new URL("./apple-touch-icon.png", import.meta.url)
+export const AppleTouchIcon = new URL(
+  "./assets/logos/apple-touch-icon.png",
+  import.meta.url
+).href;
+export const Favicon = new URL("./assets/logos/favicon.ico", import.meta.url)
   .href;
-export const Favicon = new URL("./assets/favicon.ico", import.meta.url).href;
-export const Logo192 = new URL("./assets/icon-192.png", import.meta.url).href;
-export const Logo32 = new URL("./assets/icon-32.png", import.meta.url).href;
-export const Logo512 = new URL("./assets/icon-512.png", import.meta.url).href;
-export const Logo600 = new URL("./assets/icon-600.png", import.meta.url).href;
+export const Logo192 = new URL("./assets/logos/icon-192.png", import.meta.url)
+  .href;
+export const Logo32 = new URL("./assets/logos/icon-32.png", import.meta.url)
+  .href;
+export const Logo512 = new URL("./assets/logos/icon-512.png", import.meta.url)
+  .href;
+export const Logo600 = new URL("./assets/logos/icon-600.png", import.meta.url)
+  .href;
