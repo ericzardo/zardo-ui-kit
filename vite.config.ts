903f1ed3e64e6725fcd5d8e57ba2de7fd868024e
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-04-04 15:45:20 -0300

    remove tailwindcss build

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,10 @@ export default defineConfig({
         {
           src: "src/styles/theme.css",
           dest: "styles"
+        },
+        {
+          src: "src/styles/globals.css",
+          dest: "styles"
         }
       ],
     }),
